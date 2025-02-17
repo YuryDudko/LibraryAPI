0a297f48ca95f583a476d116d7e9d08d6fcbd0e5
Yury <dudcko.yury2016@yandex.ru>
2025-02-17 20:07:12 +0300

    Docker build addition

--- LibraryWebApi/Program.cs
+++ LibraryWebApi/Program.cs
@@ -21,7 +21,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
+        builder.WebHost.UseUrls("http://0.0.0.0:8080");
         builder.Services.AddControllers();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
